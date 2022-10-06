e627749ee4e4a7b834bcfcf2532a5eb5fa2c1435
kisujin <112467643+kisujin@users.noreply.github.com>
2022-10-06 18:39:08 +0900

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,8 @@
 # Outputs file
-output "catapp_url" {
-  value = "http://${aws_eip.hashicat.public_dns}"
+output "EC2_PRIVATE_IP" {
+  value = aws_instance.terraform_kisujin_ec2.private_ip
 }
 
-output "catapp_ip" {
-  value = "http://${aws_eip.hashicat.public_ip}"
-}
+# output "catapp_ip" {
+#  value = "http://${aws_eip.hashicat.public_ip}"
+# }
