4b44cf8f91cd263c46716fc713f413f10be6557f
kisujin <112467643+kisujin@users.noreply.github.com>
2022-10-06 18:39:43 +0900

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,50 +1,34 @@
-##############################################################################
-# Variables File
-#
-# Here is where we store the default values for all the variables used in our
-# Terraform code. If you create a variable with no default, the user will be
-# prompted to enter it (or define it via config file or command line flags.)
-
-variable "prefix" {
-  description = "This prefix will be included in the name of most resources."
+variable "vpc_cidr" {
+  description = "vpc_cidr"
+  default     = "10.0.0.0/21"
 }
 
-variable "region" {
-  description = "The region where the resources are created."
-  default     = "us-east-1"
+variable "vpc_id" {
+  description = "vpc_id"
+  default     = "vpc-0469926202f01dd39"
 }
 
-variable "address_space" {
-  description = "The address space that is used by the virtual network. You can supply more than one address space. Changing this forces a new resource to be created."
-  default     = "10.0.0.0/16"
+variable "private_subnet_id" {
+  description = "subnet_id"
+  default     = "subnet-0fd2f9f72c0b9781b"
 }
 
-variable "subnet_prefix" {
-  description = "The address prefix to use for the subnet."
-  default     = "10.0.10.0/24"
+variable "nickname" {
+  description = "nickname"
+  default     = ""
 }
 
-variable "instance_type" {
-  description = "Specifies the AWS instance type."
-  default     = "t2.micro"
+variable "environment" {
+  description = "environment"
+  default     = "dev"
 }
 
-variable "admin_username" {
-  description = "Administrator user name for mysql"
-  default     = "hashicorp"
-}
+# variable "prefix" {
+#  description = "prefix"
+#  default     = "kisujin"
+# }
 
-variable "height" {
-  default     = "400"
-  description = "Image height in pixels."
-}
-
-variable "width" {
-  default     = "600"
-  description = "Image width in pixels."
-}
-
-variable "placeholder" {
-  default     = "placekitten.com"
-  description = "Image-as-a-service URL. Some other fun ones to try are fillmurray.com, placecage.com, placebeard.it, loremflickr.com, baconmockup.com, placeimg.com, placebear.com, placeskull.com, stevensegallery.com, placedog.net"
-}
+# variable "region" {
+#  description = "region"
+#  default     = "ap-northease-2"
+# }
